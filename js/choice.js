c9b31000fac46417fc104ef8972574db366482a2
Viktoriya08 <victoria.gnezdilova@gmail.com>
2020-10-27 14:29:46 +0300

    JS

--- js/choice.js
+++ js/choice.js
@@ -1,9 +1,27 @@
 const choiceButton = document.querySelector(".choice-button");
 const modalPopup = document.querySelector(".modal");
+const form = modal.querySelector (".modal-search");
+const arrival = modal.querySelector (".data-arrival");
+const departure = modal.querySelector (".data-departure");
+const parents = modal.querySelector (".parents");
+const children = modal.querySelector (".children");
+const storageParents = localStorage.getItem("parents");
+const storageChildren = localStorage.getItem("children");
+
+let isStorageSupport = true;
+let storage = "";
+
+try {
+    storage = localStorage.getItem("parents");
+  } catch (err) {
+    isStorageSupport = false;
+  }
 
 choiceButton.addEventListener("click", function (evt) {
-    evt.preventDefault();
+    if (modalPopup.classList.contains("modal")){
+        evt.preventDefault();
     modalPopup.classList.add("modal-hidden");
+    }
 });
 
 window.addEventListener("keydown", function (evt) {
@@ -15,9 +33,12 @@ window.addEventListener("keydown", function (evt) {
     }
   });
 
-/*    choiceButton.addEventListener("click", function (evt) {
-    if (modalPopup.classList.contains("modal-show")) {
+form.addEventListener("submit", function (evt) {
+    if (!arrival.value || !departure.value || !parents.value || !children.value) {
         evt.preventDefault();
-        modalPopup.classList.remove("modal-show");
+      }  else {
+        localStorage.setItem("children", children.value);
+        localStorage.setItem("parents", parents.value);
       }
-}); */
+});
+
