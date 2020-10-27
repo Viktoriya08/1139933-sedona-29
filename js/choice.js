7e645c9ce204cabe720b1e2f6a9028a91c00a7c8
Viktoriya08 <victoria.gnezdilova@gmail.com>
2020-10-27 12:28:16 +0300

    Оживляем интерактивные элементы

--- js/choice.js
+++ js/choice.js
@@ -3,21 +3,21 @@ const modalPopup = document.querySelector(".modal");
 
 choiceButton.addEventListener("click", function (evt) {
     evt.preventDefault();
-    modalPopup.classList.add("modal-show");
+    modalPopup.classList.add("modal-hidden");
 });
 
 window.addEventListener("keydown", function (evt) {
     if (evt.keyCode === 27) {
-      if (modalPopup.classList.contains("modal-show")) {
+      if (modalPopup.classList.contains("modal")) {
         evt.preventDefault();
-        modalPopup.classList.remove("modal-show");
+        modalPopup.classList.add("modal-hidden");
       }
     }
   });
 
-   choiceButton.addEventListener("click", function (evt) {
+/*    choiceButton.addEventListener("click", function (evt) {
     if (modalPopup.classList.contains("modal-show")) {
         evt.preventDefault();
         modalPopup.classList.remove("modal-show");
       }
-});
+}); */
